a635893947e940e6e0c0052381e58b92a47fb937
Anna Zhang <annazhang343@gmail.com>
2023-03-01 20:27:24 +1100

    added insights

--- popup.js
+++ popup.js
@@ -11,13 +11,56 @@ const ratingColor = (value) => {
 
 chrome.tabs.query({ active: true }).then((tabs) => getProfile(tabs));
 
+function Insight({ icon, text }) {
+  const container = document.createElement('div');
+  container.classList.add("insightContainer");
+
+  const image = new Image(40, 40);
+  image.src = icon;
+  image.alt = "Picture of the author";
+
+  const paragraph = document.createElement('p');
+  paragraph.innerText = text;
+
+  container.appendChild(image);
+  container.appendChild(paragraph);
+  return container;
+}
+
+function Insights({ profile }) {
+  const insights = [];
+
+  if (profile && profile.verified) {
+    insights.push(Insight({ icon: "images/tick.svg", text: "Manually Verified" }));
+  }
+
+  return insights;
+}
+
 const getProfile = (tabs) => {
   const div = document.getElementById('value');
   const url = tabs[0].url;
   getProfileData(url).then((profile) => {
     if (profile != '') {
       div.style.color = ratingColor(profile.value);
-      div.innerHTML = `${profile.value}`;
+      div.innerHTML = profile.ratings && profile.ratings.length ? `${profile.value}` : "—";
+
+      const noInsights = document.createElement('div');
+      noInsights.classList.add("noInsights");
+      const p = document.createElement('p');
+      p.innerText = 'Insights available with more data';
+      noInsights.appendChild(p);
+
+      const insights = Insights({ profile });
+      const withInsights = document.createElement('div');
+      withInsights.classList.add("insightsContainer");
+
+      for (let i = 0; i < insights.length; i++) {
+        withInsights.appendChild(insights[i]);
+      }
+
+      document.getElementById('container').appendChild(insights.length > 0 ? withInsights : noInsights)      
+
       document.getElementById('header').addEventListener('click', () => { redirect(profile.name) });
     } else {
       div.innerHTML = `failed`
